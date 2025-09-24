b2c5f5b716978da866b97fff5bd08c5edb29a500
Ayazalidharejo <Ayazali.xwave@gmail.com>
2025-09-24 17:07:15 +0500

    change

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,3 @@
-// import { Toaster } from "@/components/ui/toaster";
-// import { Toaster as Sonner } from "@/components/ui/sonner";
-// import { TooltipProvider } from "@/components/ui/tooltip";
-// import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-// import { BrowserRouter, Routes, Route } from "react-router-dom";
-// import Home from "./pages/Home";
-// import Tours from "./pages/Tours";
-// import Destinations from "./pages/Destinations";
-// import DestinationDetail from "./pages/DestinationDetail";
-// import Contact from "./pages/Contact";
-// import TourGuide from "./pages/TourGuide";
-// import AdminLogin from "./pages/AdminLogin";
-// import AdminDashboard from "./pages/AdminDashboard";
-// import NotFound from "./pages/NotFound";
-
-// const queryClient = new QueryClient();
-
-// const App = () => (
-//   <QueryClientProvider client={queryClient}>
-//     <TooltipProvider>
-//       <Toaster />
-//       <Sonner />
-//       <BrowserRouter>
-//         <Routes>
-//           <Route path="/" element={<Home />} />
-//           <Route path="/tours" element={<Tours />} />
-//           {/* <Route path="/destinations" element={<Destinations />} /> */}
-//           <Route path="/destinations/:id" element={<DestinationDetail />} />
-//           <Route path="/contact" element={<Contact />} />
-//           <Route path="/guide" element={<TourGuide />} />
-//           <Route path="/admin/login" element={<AdminLogin />} />
-//           <Route path="/admin/dashboard" element={<AdminDashboard />} />
-//           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-//           <Route path="*" element={<NotFound />} />
-//         </Routes>
-//       </BrowserRouter>
-//     </TooltipProvider>
-//   </QueryClientProvider>
-// );
-
-// export default App;
-
 
 
 import { Toaster } from "@/components/ui/toaster";
@@ -47,10 +5,10 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Header from "./components/layout/Header"; // 👈 import here
+import Header from "./components/layout/Header"; 
 import Home from "./pages/Home";
 import Tours from "./pages/Tours";
-// import Destinations from "./pages/Destinations";
+
 import DestinationDetail from "./pages/DestinationDetail";
 import Contact from "./pages/Contact";
 import TourGuide from "./pages/TourGuide";
@@ -62,7 +20,6 @@ import ShortFormDetail from "./components/ShortFormDetail";
 import BlogList from "./pages/Home";
 import ToursList from "./pages/ToursList";
 import TourDetail from "./pages/TourDetail";
-// import AdminTours from "./pages/Think";
 import TourForm from "./pages/TourForm";
 
 const queryClient = new QueryClient();
@@ -73,7 +30,7 @@ const App = () => (
       <Toaster />
       <Sonner />
       <BrowserRouter>
-        <Header />   {/* 👈 Added here */}
+        <Header />   
         <Routes>
             <Route path="/tours" element={<ToursList />} />
         <Route path="/tours/:id" element={<TourDetail />} />
@@ -83,8 +40,8 @@ const App = () => (
         <Route path="/admin/tours/new" element={<TourForm />} />
         <Route path="/admin/tours/edit/:id" element={<TourForm />} />
           <Route path="/" element={<Home />} />
-          <Route path="/" element={<BlogList />} />
-        <Route path="/blogs/:id" element={<BlogDetail />} />
+         
+        {/* <Route path="/blogs/:id" element={<BlogDetail />} /> */}
            <Route path="/detail/:id" element={<BlogDetail />} />
           <Route path="/tours" element={<Tours />} />
            <Route path="/short-form/:id" element={<ShortFormDetail />} />
